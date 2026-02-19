3714d96aea80b86e2b0bbfe21f20aeea66196a08
Kumar Dahal <kudahal007@gmail.com>
2026-02-18 21:23:23 -0500

    spring-boot project initialization

---
+++ src/main/java/np/dev/kd/springbootdemoapplication/SpringbootdemoapplicationApplication.java
@@ -0,0 +1,13 @@
+package np.dev.kd.springbootdemoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/np/dev/kd/springbootdemoapplication/SpringbootdemoapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package np.dev.kd.springbootdemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
